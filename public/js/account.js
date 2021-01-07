65c9c0271c64070c6918c0d8f1d284d6b64b48a3
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-06 20:08:26 -0600

    updated navbar menu to be responsive

--- public/js/account.js
+++ public/js/account.js
@@ -6,4 +6,13 @@ $(document).ready(() => {
   $.get('/api/user_data').then((data) => {
     $('.member-name').text(data.email);
   });
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
 });
