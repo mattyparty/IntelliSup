1ee412eb13f185368a36ccbc62ffd7310bfe3ff2
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-06 23:52:11 -0600

    additional styling on homepage and navbar

--- public/js/account.js
+++ public/js/account.js
@@ -15,4 +15,20 @@ $(document).ready(() => {
     $(".navbar-menu").toggleClass("is-active");
 
   });
+
+  $('.tabs').each(function (index) {
+    var $tabParent = $(this);
+    var $tabs = $tabParent.find('li');
+    var $contents = $tabParent.find('.tab-content');
+
+    $tabs.click(function () {
+      var curIndex = $(this).index();
+      // toggle tabs
+      $tabs.removeClass('is-active');
+      $tabs.eq(curIndex).addClass('is-active');
+      // toggle contents
+      $contents.removeClass('is-active');
+      $contents.eq(curIndex).addClass('is-active');
+    });
+  });
 });
