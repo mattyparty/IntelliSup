6283036aa4779bf420def1b73cf3541527c90bfe
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-07 11:51:25 -0600

    added testimonials using handlebars

--- routes/html/index.js
+++ routes/html/index.js
@@ -4,7 +4,33 @@ const router = require('express').Router();
 
 // Route to render home page
 router.get('/', (req, res) => {
-  res.render('index', { home: true });
+  res.render('index',
+    {
+      home: true,
+      testimonials: [
+        {
+          name: "L. Luthor",
+          title: "Billionaire Mastermind",
+          quote: "Kryptonite is crucial for my operation on a day to day basis. IntelliSup has allowed me to track my suppliers until the parts show up to my laye... um business... I am truly appreciative of their product!"
+        },
+        {
+          name: "J. Gordon",
+          title: "Commissioner",
+          quote: "Spotlight bulbs burn very quickly at high elevation. Keeping a lot of stock on hand is crucial to our operation here in Gotham. IntelliSup aides us in making sure our shelves are stocked and informs us when the next shipment will arrive."
+        },
+        {
+          name: "S. Rogers",
+          title: "Captain",
+          quote: "In my line of work, I am constantly having to re-paint my shield. Vibranium is an extremely hard metal to bond paint to, so I need the right kind. IntelliSup has made it so easy to know exactly when the right paint will arrive, and I can hold the red paint supplier just as accountable as the blue and white ones. Thank you IntelliSup!"
+        },
+        {
+          name: "HULK SMASH",
+          title: "SCIENTIST",
+          quote: "ME NEED FOOD AND WATER TO SURVIVE! MANY TIME I CAUSED AGGRAVATION FROM SUPPLIER WHO NOT MEET MY REQUIREMENT! INTELLISUP HELP ME AND FILL MY STOMACH ARRRRRRRRRRRRRRRRRRRRRRR!!!"
+        }
+      ],
+    }
+  );
 });
 
 // Route to render login?
